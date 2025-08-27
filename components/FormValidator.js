bd4094eabda1635caa87532f823f41c96ff778ef
Yessica Rodocker <yessica.rodocker@gmail.com>
2025-08-27 17:17:08 -0500

    add toggleButtonState, hasInvalidInput, checkInputValidity

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -7,7 +7,56 @@ class FormValidator {
     this._inactiveButtonClass = settings.inactiveButtonClass;
     this._formEl = formEl;
   }
-  enableValidation() {}
+
+  _checkInputValidity() {
+    if (!inputElement.validity.valid) {
+      showInputError(formEl, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formEl, inputElement);
+    }
+  }
+
+  _hasInvalidInput() {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _toggleButtonState() {
+    if (hasInvalidInput(inputList)) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _setEventListeners() {
+    const inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    this._buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
+
+    this._toggleButtonState(this._inputSelector);
+
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        checkInputValidity(inputElement);
+        this._toggleButtonState(this._inputSelector);
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+    this._toggleButtonState();
+  }
 }
 
 export default FormValidator;
